115b3a2a60b1088bdbf988af89dbe9c58fb4e0ca
Fayaz-21 <fayaztunio06@gmail.com>
2023-10-05 00:05:13 +0500

    signIn Created

--- api/controlers/auth.controller.js
+++ api/controlers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from "bcryptjs"
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken'
 export const signup=async(req,res,next)=>{
   const {username,email,password} = req.body;
   const hashedPassword =bcryptjs.hashSync(password,10)
@@ -16,4 +17,22 @@ export const signup=async(req,res,next)=>{
   }
 
 
+}
+
+export const signin=async(req,res,next)=>{
+  const {email,password} = req.body;
+  try {
+    const validUser=await User.findOne({email})
+    if(!validUser)return next(errorHandler(404,'User Not Found')) 
+    const validPassword=bcryptjs.compareSync(password,validUser.password)
+    console.log(validUser.password)
+  if(!validPassword)return next(errorHandler(401,'Wrong credentials'))
+  const token=jwt.sign({id:validUser._id},process.env.JWT_SECRETE)
+  const {password:pass,...rest}=validUser._doc
+  res.cookie('access_token',token,{httpOnly:true}).status(200).json(validUser)
+  console.log(validUser)
+  } catch (error) {
+    next(error)
+    
+  }
 }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import { signin } from '../controlers/auth.controller.js';
 const router=express.Router();
 import { signup } from '../controlers/auth.controller.js';
 router.post('/signup', signup)
+router.post('/signin',signin)
 export default router;
