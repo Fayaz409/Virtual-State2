204045bc47eaf17583769323f2e4eee4c263f656
Fayaz-21 <fayaztunio06@gmail.com>
2023-10-05 12:01:44 +0500

    complete sign in functionality

--- api/controlers/auth.controller.js
+++ api/controlers/auth.controller.js
@@ -19,20 +19,24 @@ export const signup=async(req,res,next)=>{
 
 }
 
-export const signin=async(req,res,next)=>{
-  const {email,password} = req.body;
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
   try {
-    const validUser=await User.findOne({email})
-    if(!validUser)return next(errorHandler(404,'User Not Found')) 
-    const validPassword=bcryptjs.compareSync(password,validUser.password)
-    console.log(validUser.password)
-  if(!validPassword)return next(errorHandler(401,'Wrong credentials'))
-  const token=jwt.sign({id:validUser._id},process.env.JWT_SECRETE)
-  const {password:pass,...rest}=validUser._doc
-  res.cookie('access_token',token,{httpOnly:true}).status(200).json(validUser)
-  console.log(validUser)
+    const validUser = await User.findOne({ email });
+    console.log(validUser)
+    if (!validUser) return next(errorHandler(404, 'User not found!'));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    console.log(`password: ${validPassword} and password ${password}`)
+    if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+    console.log('here')
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    console.log(`token: ${token}`);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie('access_token', token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
-    next(error)
-    
+    next(error);
   }
-}
+};
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import { signin } from '../controlers/auth.controller.js';
+//import { signin } from '../controlers/auth.controller.js';
 const router=express.Router();
-import { signup } from '../controlers/auth.controller.js';
+import { signup,signin } from '../controlers/auth.controller.js';
 router.post('/signup', signup)
-router.post('/signin',signin)
+router.post("/signin", signin);
 export default router;
